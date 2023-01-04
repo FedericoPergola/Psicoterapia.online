8d6848e5ccba78e9957a2010c5475e9dea2e3a40
Federico Pergola <fede.pergola@hotmail.com>
2023-01-03 23:28:45 -0300

    Feat | Component Team

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar/NavBar';
+import Team from './components/Team/Team';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Team />
     </div>
   );
 }
---
+++ src/components/Team/Team.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import './Team.css'
+import natalia from '../../assests/natalia.png'
+import melisa from '../../assests/melisa.png'
+import luciana from '../../assests/luciana.png'
+
+const Team = () => {
+  return (
+    <div className='team-content'>
+        <div className=' title-team d-flex justify-content-center pt-5'>
+            <h2>Conoce a nuestro equipo</h2>
+        </div>
+        <div className='Team d-flex  justify-content-evenly'>
+            <div class="card-content w-25">
+                <div className='d-flex justify-content-center pt-3'>
+                    <img className='img card-img-top' src={natalia} alt="..."/> 
+                </div>
+                <div class="card-body pt-5">
+                    <p className='text-lic'>Lic. Natalia Brunati</p>
+                    <p>Licenciada en Psicología</p>
+                </div>
+            </div>
+            <div class="card-content-melisa w-25">
+                <div className='d-flex justify-content-center pt-3'>
+                    <img className='img card-img-top' src={melisa} alt="..."/> 
+                </div>
+                <div class="card-body pt-5">
+                    <p className='text-lic'>Lic. Melisa Condello</p>
+                    <p>Licenciada en Psicología</p>
+                </div>
+            </div>
+            <div class="card-content w-25">
+                <div className='d-flex justify-content-center pt-3'>
+                    <img className='img card-img-top' src={luciana} alt="..."/> 
+                </div>
+                <div class="card-body pt-5">
+                    <p className='text-lic'>Lic. Luciana Schammah</p>
+                    <p>Licenciada en Psicología</p>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Team
